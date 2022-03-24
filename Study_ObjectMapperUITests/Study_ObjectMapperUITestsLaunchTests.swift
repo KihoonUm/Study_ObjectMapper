68ed79f7f2c9b0c70fd8ee621c94746abd58e7a5
Bard <rhrnrkd@naver.com>
2022-03-24 11:32:32 +0900

    Initial Commit

---
+++ Study_ObjectMapperUITests/Study_ObjectMapperUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Study_ObjectMapperUITestsLaunchTests.swift
+//  Study_ObjectMapperUITests
+//
+//  Created by Bard on 2022/03/24.
+//
+
+import XCTest
+
+class Study_ObjectMapperUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
